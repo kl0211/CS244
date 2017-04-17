ee3babc94432ef59e77146ac475aed9731f2b2bd
Rob Werfelmann <rob@werfelmann.com>
2017-04-17 15:05:50 +0300

    Added tests for random RED queue

--- testscript.sh
+++ testscript.sh
@@ -4,6 +4,6 @@ do
     sed -i "s/set val(ifqlen) [0-2][0-9]/set val(ifqlen) ${num}/g" wireless.tcl
     for i in {1 2 3 4 5}
     do
-        ns wireless.tcl && ./ns2stats.py out.tr >> stats.DropTail.ran${num}.txt
+        ns wireless.tcl && ./ns2stats.py out.tr >> stats.RED.ran${num}.txt
     done
 done
